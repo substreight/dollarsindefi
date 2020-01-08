ce2fdd665d8cca79982f1affac1cf15a5f51297b
substreight <52869376+substreight@users.noreply.github.com>
2020-01-08 16:28:12 -0700

    Add files via upload

--- defiTest.js
+++ defiTest.js
@@ -1,20 +1,16 @@
-/*
-fetch('https://public.defipulse.com/api/MarketData')
-  .then(response => response.json())
-  .then(data => (document.getElementById('totalUSD').textContent = data.All.total))
-  .catch(error => console.error(error));
-*/
-
-window.addEventListener("load", () => {
+const callApi = async () => {
 
 const numberFormat = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
-
-setInterval(async () => {
-
-const response = await fetch("https://public.defipulse.com/api/MarketData");
-const { All: { total } } = await response.json();
-document.getElementById("totalUSD").textContent = numberFormat.format(total);
-
-}, 5000);
-
-} )
+  const response = await fetch("https://public.defipulse.com/api/MarketData");
+  if (response.ok) {
+    let { All: { total } } = await response.json(); document.getElementById('totalUSD').innerHTML =  numberFormat.format(total);
+    setTimeout(() => {
+      callApi();
+    }, 180000); // wait for one minute before calling the api again
+  } else {
+    callApi();
+  }
+};
+window.addEventListener("load", () => {
+  callApi();
+});
